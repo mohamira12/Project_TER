6cca8d21ff1d245f49191fdc79fe456067997e8e
Amira Mohammedi <amira.mohammedi@etu.univ-grenoble-alpes.fr>
2024-03-27 11:03:15 +0100

    Aws Authentification à complete

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react-native';
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
+import config from './src/aws-exports';
 import Navigation from './src/navigation';
-export default function App() {
 
+Amplify.configure(config);
+const  App=()=> {
+  //Auth.signOut();
   return (
     <SafeAreaView style={styles.container}>
     
@@ -20,3 +25,44 @@ const styles = StyleSheet.create({
     backgroundColor: '#F9FBFC'
     }
   });
+const SignUpConfig={
+  header:"Welcome to the SignUp",
+  hideAllDefaults: true,
+ 
+ signUpFields:[
+      {
+        label:'Full Name',
+        key:'name',
+        required:true,
+        displayOrder:1,
+        type:"string",
+      },
+      {
+        label:'Email',
+        key:'email',
+        required:true,
+        displayOrder:2,
+        type:"string",
+
+      },
+      {
+        label:'Username',
+        key:'username',
+        required:true,
+        displayOrder:3,
+        type:"string",
+      }
+      ,
+      {
+        label:'Password',
+        key:'password',
+        required:true,
+        displayOrder:4,
+        type:"password",
+      },
+     
+       
+    ],
+  
+};
+export default withAuthenticator(App,{ signUpConfig: SignUpConfig});
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "socialMediaTerR": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
