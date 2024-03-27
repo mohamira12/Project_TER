4ef56e33bc55312f1f5a915aea320c5fc6f00c18
Amira Mohammedi <amira.mohammedi@etu.univ-grenoble-alpes.fr>
2024-03-27 11:08:54 +0100

    CostumTheme

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { Amplify } from 'aws-amplify';
-import { withAuthenticator } from 'aws-amplify-react-native';
+import { AmplifyTheme, withAuthenticator } from 'aws-amplify-react-native';
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import config from './src/aws-exports';
@@ -65,4 +65,8 @@ const SignUpConfig={
     ],
   
 };
-export default withAuthenticator(App,{ signUpConfig: SignUpConfig});
+const CostumTheme={
+...AmplifyTheme,
+
+};
+export default withAuthenticator(App,{ signUpConfig: SignUpConfig,theme:CostumTheme});
